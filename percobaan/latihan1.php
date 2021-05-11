bfb52ce8dae527253ced17b17b690609a92a7c11
wert000 <ertanto4@gmail.com>
2021-05-11 16:25:57 +0700

    menambahkan gambar

--- percobaan/latihan1.php
+++ percobaan/latihan1.php
@@ -9,7 +9,22 @@
 </head>
 
 <body>
+  <h3>Daftar Mahasiswa</h3>
 
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src=""></td>
+    </tr>
+  </table>
 </body>
 
 </html>
